23083c4be45c854db80961dc986c027512a628eb
Pablo Griscti <pablo.griscti@imagination.com>
2015-05-18 23:12:06 +0100

    Updates constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,16 @@
 weatherApp.constant('constants', {
+	config: {
+		apiUrl: 'http://api.openweathermap.org/data/2.5/forecast/daily'
+	},
 	defaultValues: {
 		forecastNumDays: 5
+	},
+	events: {
+		newForecastData: 'newForecastData'
+	},
+	pages: {
+		home: '/',
+		forecast: '/forecast'
 	},
 	strings: {
 		forecastCity: 'Forecast by city',
